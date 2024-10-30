1311bf9c5d735c3fc6f94fd0e8a48f057b390529
vlenel <victor.lenel@epita.fr>
2024-10-30 13:28:08 +0100

    fixed ci to test backend also

--- backend/routes/shoppingList.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getShoppingList } = require('../controllers/shoppingListController');
-
-router.get('/', getShoppingList);
-
-module.exports = router;
