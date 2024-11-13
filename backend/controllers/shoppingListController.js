7ecd76218f931ce47742543a2e3b484597665beb
leo.verniquet <leo.verniquet@epita.fr>
2024-11-13 16:41:21 +0100

    feat: backend can now create list with IA

--- backend/controllers/shoppingListController.js
+++ backend/controllers/shoppingListController.js
@@ -2,11 +2,22 @@ const axios = require('axios');
 const db = require('../db');
 require('dotenv').config();
 
-async function getShopArticles(req, res) { // TODO pas un endpoint une fonction
+async function getShopArticles(req, res) {
   try {
-    const result = await db.query('SELECT * FROM list_article where user_id = 1');
-    console.log(result.rows);
+    const result = await db.query('SELECT * FROM shop_articles;');
+    //console.log(result.rows);
     res.status(200).json(result.rows);
+  }
+  catch (error) {
+    console.error("Error fetching from DB:", error.response ? error.response.data : error.message);
+    res.status(500).json({ error: 'Failed to retrieve shopping list from database' });
+  }
+}
+
+async function getShopArticlesForAI() {
+  try {
+    const result = await db.query('SELECT id, name, brand, price FROM shop_articles;');
+    return(result.rows)
   }
   catch (error) {
     console.error("Error fetching from DB:", error.response ? error.response.data : error.message);
@@ -17,14 +28,16 @@ async function getShopArticles(req, res) { // TODO pas un endpoint une fonction
 
 async function insertListArticle(userId, article) {
   const query = `
-    INSERT INTO list_article (user_id, name, active, scanned)
-    VALUES ($1, $2, $3, $4)
+    INSERT INTO list_article (user_id, name, active, scanned, article_id)
+    VALUES ($1, $2, $3, $4, $5)
     RETURNING id
   `;
-  const values = [userId, article.name, true, false]; // Supposons que l'article est actif par défaut et non scanné
+  const values = [userId, article.name, true, false, article.id]; // Supposons que l'article est actif par défaut et non scanné
 
   try {
+    // console.log(query)
     const result = await db.query(query, values);
+    // console.log(result.rows[0]);
     return result.rows[0].id;
   } catch (err) {
     console.error('Erreur lors de l\'insertion de l\'article', err);
@@ -34,12 +47,14 @@ async function insertListArticle(userId, article) {
 
 async function insertArticlesFromJSON(userId, articlesJSON) {
   const articles = articlesJSON.ingredients;
+  // console.log(JSON.stringify(articles))
   const insertedIds = [];
 
   for (const article of articles) {
     try {
       const id = await insertListArticle(userId, article);
       insertedIds.push(id);
+      // console.log("inserted " + article);
     } catch (err) {
       console.error(`Erreur lors de l'insertion de l'article ${article.name}`, err);
       // Décidez ici si vous voulez continuer avec les autres articles ou arrêter le processus
@@ -49,7 +64,15 @@ async function insertArticlesFromJSON(userId, articlesJSON) {
 }
 
 async function getShoppingListFromAI(req, res) {
-  const { Recipe, Budget } = req.body;
+  const { recipe, budget, tags } = req.body;
+  const articles = JSON.stringify(await getShopArticlesForAI());
+  var tag_request = ""
+  // console.log(tags.length);
+  if (tags != null && tags.length >= 1) {
+    tag_request = `take in account my preferences : ${tags},`
+  }
+  // console.log(tag_request)
+  //console.log(articles);
   try {
     const response = await axios.post(
       `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${process.env.GOOGLE_API_KEY}`,
@@ -58,7 +81,7 @@ async function getShoppingListFromAI(req, res) {
           {
             parts: [
               {
-                  text: `Generate a french and concise JSON response with just the ingredients, the quantity and their prices for the recipe: ${Recipe}. Format it as: {"ingredients": [{"name": "ingredient", "price": "price", "quantity": "quantity"}]}. No additional explanations or notes.`
+                  text: `Generate a concise JSON response with just the ingredients from this product database : ${articles},${tag_request} the quantity and their prices for the following french recipe: ${recipe}. Format it as: {"ingredients": [{"id": int, "name": "ingredient", "price": "price", "quantity": "quantity"}]}. No additional explanations or notes.`
               }
             ]
           }
@@ -67,23 +90,37 @@ async function getShoppingListFromAI(req, res) {
       { headers: { 'Content-Type': 'application/json' } }
       );
 
+      // console.log(response.data.candidates[0].content.parts[0])
       const generatedText = response.data.candidates[0].content.parts[0].text;
-      //console.log("Generated Text:", generatedText);
 
       const cleanText = generatedText.replace(/```json|```|\n/g, '').trim();
       const generatedJson = JSON.parse(cleanText);
 
-      // ecraser la liste de course de l'user 1
-      await db.query("DELETE FROM list_article WHERE user_id = 1");
+      // console.log(generatedJson);
 
-      insertArticlesFromJSON(1, generatedJson);
-  
-      const result = await db.query('SELECT * FROM list_article where user_id = 1');
-      res.status(200).json(result.rows);
+      const result = await updateUserShoppingList(generatedJson);
+      // console.log(result)
+
+      res.status(200).json(result);
       //res.status(200).json(generatedJson.ingredients);
   } catch (error) {
     console.error("Error fetching from API:", error.response ? error.response.data : error.message);
     res.status(500).json({ error: 'Failed to retrieve shopping list from AI' });
+  }
+}
+
+
+async function updateUserShoppingList(input) {
+  try {
+    await db.query("DELETE FROM list_article WHERE user_id = 1");
+    await insertArticlesFromJSON(1, input);
+    const result = await db.query('SELECT * FROM list_article where user_id = 1');
+    //console.log(result.rows)
+    return result.rows;
+  }
+  catch (error){
+    console.error("Error fetching from API:", error.response ? error.response.data : error.message);
+    res.status(500).json({ error: 'Failed to update user list' });
   }
 }
 
@@ -99,5 +136,5 @@ async function getShoppingList(req, res) {
   }
 }
 
-module.exports = { getShoppingListFromAI, getShoppingList };
+module.exports = { getShoppingListFromAI, getShoppingList, getShopArticles };
 
--- backend/routes/shoppingList.js
+++ backend/routes/shoppingList.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getShoppingListFromAI, getShoppingList } = require('../controllers/shoppingListController');
+const { getShoppingListFromAI, getShoppingList, getShopArticles} = require('../controllers/shoppingListController');
 
+router.get('/shop_articles/', getShopArticles);
 router.post('/:id', getShoppingListFromAI);
 router.get('/:id', getShoppingList);
+
 
 module.exports = router;
