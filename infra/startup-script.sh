3753ae718b4af97b2f8446e819f57d6118296572
victor.lenel <victor.lenel@epita.fr>
2025-01-29 11:53:28 +0100

    fix: reset the git repo and pull it

--- infra/startup-script.sh
+++ infra/startup-script.sh
@@ -6,13 +6,15 @@ sudo apt update
 
 # Define repertory of project and move in
 REPO_DIR="/home/debian/IoT_Backend/"
-cd "$REPO_DIR"
 
 # Configure Git to explicitly mark the directory as safe
 git config --global --add safe.directory /home/debian/IoT_Backend
 
 # Pulling github backend repository
-git pull
+cd ~
+sudo rm -rf ~/IoT_Backend
+git clone https://github.com/6-great-leaders/IoT_Backend.git
+cd IoT_Backend
 
 # Clean up old containers
 sudo docker compose -f "$REPO_DIR/docker-compose.yaml" down
